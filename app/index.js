3899dae87f07597962efced1d52d4a50389a481d
Maicol Oliveira <maicol20000@gmail.com>
2024-01-18 21:20:18 -0300

    proxy reverse

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("teste v33 44s");
+  res.send("teste docker proxy reverse!");
 });
 
 app.listen(3000);
